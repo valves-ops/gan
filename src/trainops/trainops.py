09279bb641387576754745c9674dbec7cdd2fd7a
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-04-21 21:26:38 +0000

    Trainops WIP

--- src/trainops/trainops.py
+++ src/trainops/trainops.py
@@ -13,24 +13,22 @@ class GANTrainOps:
         input_function,
         model_slug,
         epochs=100,
-        batches_per_checkpoint=100,
-        batches_per_evaluation=100,
+        epochs_per_checkpoint=5,
+        batches_per_evaluation=500,
         batch_count_for_evalution=10,
     ):
         self.gan_estimator = gan_estimator
         self.input_function = input_function
         self.model_slug = model_slug
         self.epochs = epochs
-        self.batches_per_checkpoint = batches_per_checkpoint
+        self.epochs_per_checkpoint = epochs_per_checkpoint
         self.batches_per_evaluation = batches_per_evaluation
         self.batch_count_for_evalution = batch_count_for_evalution
 
-        print(self._get_checkpoint_prefix())
-
         self.checkpoint_manager = tf.train.CheckpointManager(
-            self.gan_estimator.checkpoint, 
-            self._get_checkpoint_directory(), 
-            max_to_keep=3
+            self.gan_estimator.checkpoint,
+            self._get_checkpoint_directory(),
+            max_to_keep=3,
         )
 
     def train(self):
@@ -41,26 +39,26 @@ class GANTrainOps:
         # Create metrics buffers
         metrics_buffer = {}
         for metric_name in self.gan_estimator.evaluation_metrics:
-            metrics_buffer.update({ metric_name: [] })
+            metrics_buffer.update({metric_name: []})
 
         for epoch in range(self.epochs):
             batches = self.input_function(mode="train")
             batch_number = 0
             batch_durations = []
             for batch in batches:
+                batch_number += 1
+
                 batch_start_time = time.time()
 
                 self.gan_estimator.train_step(batch)
 
                 batch_end_time = time.time()
-
-                batch_number += 1
                 batch_durations.append(batch_end_time - batch_start_time)
 
-                if batch_number % self.batches_per_evaluation:
+                if batch_number % self.batches_per_evaluation == 0:
                     # Evaluate
-                    metrics = self.gan_estimator.evaluate(self.input_function, self.batch_count_for_evalution)
-                    
+                    # metrics = self.gan_estimator.evaluate(self.input_function, self.batch_count_for_evalution)
+
                     # Console Logging
                     print(
                         "Time since start: %.2f min"
@@ -70,17 +68,20 @@ class GANTrainOps:
                     print(f"Average batch time: {np.average(batch_durations)} secs")
 
                     # Metrics Logging
-                    for metric_name, metric in metrics.items():
-                        metrics_buffer[metric_name].append(metric.result())
-                        print(f'{metric_name}: {metric.result()}')
+                    # for metric_name, metric in metrics.items():
+                    #     metrics_buffer[metric_name].append(metric.result())
+                    #     print(f'{metric_name}: {metric.result()}')
 
                     # Generate Images
                     images = self.gan_estimator.predict(base_images_latent_vectors)
-                    tfgan.eval.python_image_grid(imgs, grid_shape=(2, 10))
+                    img_grid = tfgan.eval.python_image_grid(images, grid_shape=(2, 10))
+                    plt.axis("off")
+                    plt.imshow(np.squeeze(img_grid))
+                    plt.show()
 
-                if batch_number % self.batches_per_checkpoint:
+                if epoch % self.epochs_per_checkpoint == 0:
                     self.gan_estimator.checkpoint.save(
-                        file_prefix=self.get_checkpoint_prefix()
+                        file_prefix=self._get_checkpoint_prefix()
                     )
                     # TODO: Save metrics to disk
 
