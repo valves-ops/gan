a822c6b0c28eafa705de4059c85a2eb062ff8cd7
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-05-02 20:51:56 +0000

    Remove coments

--- src/main.py
+++ src/main.py
@@ -14,37 +14,6 @@ from trainops.trainops import GANTrainOps
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-# def input_fn(mode):
-#     # assert 'batch_size' in params
-#     # assert 'noise_dims' in params
-#     bs = 256 #params['batch_size']
-#     nd = 100 #params['noise_dims']
-#     split = 'train' if mode == tf.estimator.ModeKeys.TRAIN else 'test'
-#     shuffle = (mode == tf.estimator.ModeKeys.TRAIN)
-#     just_noise = (mode == tf.estimator.ModeKeys.PREDICT)
-
-    
-
-#     if just_noise:
-#         return noise_ds
-
-#     def _preprocess(element):
-#         # Map [0, 255] to [-1, 1].
-#         images = (tf.cast(element['image'], tf.float32) - 127.5) / 127.5
-#         return images
-
-#     images_ds = (tfds.load('mnist:3.*.*', split=split)
-#                 .map(_preprocess)
-#                 .cache()
-#                 .repeat())
-#     if shuffle:
-#         images_ds = images_ds.shuffle(
-#             buffer_size=60000, reshuffle_each_iteration=True)
-#         images_ds = (images_ds.batch(bs, drop_remainder=True)
-#                     .prefetch(tf.data.experimental.AUTOTUNE))
-
-#     return tf.data.Dataset.zip((noise_ds, images_ds))
-
 
 def _preprocess(element):
         # Map [0, 255] to [-1, 1].
@@ -53,19 +22,17 @@ def _preprocess(element):
 
 @gin.configurable
 def get_dataset(BATCH_SIZE = 128, BUFFER_SIZE = 10000, NOISE_DIM = 100):
+    print('--- Dataset Setup ---')
     print('BATCH SIZE: ', BATCH_SIZE)
     print('BUFFER SIZE: ', BUFFER_SIZE)
     print('NOISE DIM: ', NOISE_DIM)
+    print('---------------------')
     noise_ds = (tf.data.Dataset.from_tensors(0).repeat().map(lambda _: tf.random.normal([BATCH_SIZE, NOISE_DIM])))
 
-    # images_ds = (tfds.load('mnist:3.*.*', split='train')
-    #                 .map(_preprocess)
-    #                 .cache()
-    #                 .repeat())
     (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
     train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-    # train_images = (train_images - 127.5) / 127.5 
-    train_images = (train_images) / 255
+    train_images = (train_images - 127.5) / 127.5 
+    # train_images = (train_images) / 255
 
     image_ds = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
