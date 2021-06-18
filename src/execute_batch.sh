84372b0baa2c88e24439e3382eca8a2d04e1dc91
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-06-18 12:57:47 +0000

    update batch execute script and fix depth combinations project name

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # /bin/bash execute_experiment.sh mnist.depth_scaling
-/bin/bash execute_experiment.sh mnist.dim_prog_combinations
+# /bin/bash execute_experiment.sh mnist.dim_prog_combinations
+# /bin/bash execute_experiment.sh mnist.filter_prog_combinations
+/bin/bash execute_experiment.sh mnist.depth_combinations
