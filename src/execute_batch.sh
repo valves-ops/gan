f637e331eafb754b9a436d1b300de867226ce334
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-06-19 19:46:58 +0000

    move completed runs to completed runs directory

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -3,4 +3,5 @@
 # /bin/bash execute_experiment.sh mnist.depth_scaling
 # /bin/bash execute_experiment.sh mnist.dim_prog_combinations
 # /bin/bash execute_experiment.sh mnist.filter_prog_combinations
-/bin/bash execute_experiment.sh mnist.depth_combinations
+# /bin/bash execute_experiment.sh mnist.depth_combinations
+/bin/bash execute_experiment.sh mnist.kernel_combinations
