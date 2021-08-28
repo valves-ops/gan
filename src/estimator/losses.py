aef6e3e49f4c7d33af969f917f4d9dd789ca4b2e
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-08-28 12:07:56 -0300

    add feature matching loss

--- src/estimator/losses.py
+++ src/estimator/losses.py
@@ -17,3 +17,15 @@ def binary_cross_entropy_generator_loss(model_output):
     y_fake = model_output['discrimination_on_generated_images']
     bce = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     return bce(tf.ones_like(y_fake), y_fake)
+
+@gin.configurable
+def feature_matching_generator_loss(model_output):
+    discriminator_feature_on_real_images = model_output['discriminator_feature_on_real_images']
+    discriminator_feature_on_generated_images = model_output['discriminator_feature_on_generated_images']
+
+    avg_discriminator_feature_on_real_images = tf.reduce_mean(discriminator_feature_on_real_images, 0)
+    avg_discriminator_feature_on_generated_images = tf.reduce_mean(discriminator_feature_on_generated_images, 0)
+
+    feature_matching_loss = tf.norm(avg_discriminator_feature_on_real_images - avg_discriminator_feature_on_generated_images)
+
+    return feature_matching_loss
