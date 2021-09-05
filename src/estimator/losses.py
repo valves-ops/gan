be7e9d2623480d656d25d125435a570c16524b86
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-09-05 12:23:07 -0300

    fix label smoothing

--- src/estimator/losses.py
+++ src/estimator/losses.py
@@ -3,14 +3,13 @@ import gin
 
 @gin.configurable
 def binary_cross_entropy_discriminator_loss(model_output, 
-                                        real_label_smoothing_factor=0.9,
-                                        fake_label_smoothing_factor=0.1):
+                                        real_label_smoothing_factor=0.9):
     
     bce = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     y_real = model_output['discrimination_on_real_images']
     y_fake = model_output['discrimination_on_generated_images']
     return bce(tf.ones_like(y_real)*real_label_smoothing_factor, y_real) \
-         + bce(tf.zeros_like(y_fake)+fake_label_smoothing_factor, y_fake)
+         + bce(tf.zeros_like(y_fake), y_fake)
 
 @gin.configurable
 def binary_cross_entropy_generator_loss(model_output):
