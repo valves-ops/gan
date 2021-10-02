7b089c99c5ac91edfec79de697c9fa74409d3b77
viniciusalves <vinicius.alves.contato@gmail.com>
2021-10-02 13:58:31 -0300

    add general frechet distance function and frechet distance function selector

--- src/trainops/metrics.py
+++ src/trainops/metrics.py
@@ -1,4 +1,16 @@
 from tensorflow_gan.examples.mnist import util as eval_util
+from tensorflow_gan.python.eval.inception_metrics import  frechet_inception_distance
 
 def frechet_distance(gan_model):
     return eval_util.mnist_frechet_distance(gan_model.real_images, gan_model.generated_images)
+
+
+def general_frechet_distance(gan_model):
+    return frechet_inception_distance(gan_model.real_images, gan_model.generated_images)
+
+
+def get_frechet_distance_func(dataset):
+    if dataset == 'MNIST':
+        return frechet_distance
+    else:
+        return general_frechet_distance
