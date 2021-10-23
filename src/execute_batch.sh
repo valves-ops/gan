2629f42ef8bc1cceae04c9559d3b5ddf1239f005
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-10-23 21:14:41 +0000

    update cifar base

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+for i in {1..3}
+do
+    /bin/bash execute_experiment.sh mnist.capacity_scaling
+done
 # /bin/bash execute_experiment.sh mnist.depth_scaling
 # /bin/bash execute_experiment.sh mnist.dim_prog_combinations
 # /bin/bash execute_experiment.sh mnist.filter_prog_combinations
 # /bin/bash execute_experiment.sh mnist.depth_combinations
-/bin/bash execute_experiment.sh mnist.kernel_combinations
+# /bin/bash execute_experiment.sh mnist.kernel_combinations
