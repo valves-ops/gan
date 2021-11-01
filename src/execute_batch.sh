f2f8f125e6b082550a31185371659fc7f5f76fdc
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-11-01 22:53:54 +0000

    remove no longer used runs and rearrange permutation files

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in {1..3}
+for i in {1..2}
 do
-    /bin/bash execute_experiment.sh mnist.capacity_scaling
+    /bin/bash execute_experiment.sh mnist.dim_prog_combinations
+    /bin/bash execute_experiment.sh mnist.filter_prog_combinations
+    /bin/bash execute_experiment.sh mnist.depth_combinations
 done
 # /bin/bash execute_experiment.sh mnist.depth_scaling
-# /bin/bash execute_experiment.sh mnist.dim_prog_combinations
-# /bin/bash execute_experiment.sh mnist.filter_prog_combinations
-# /bin/bash execute_experiment.sh mnist.depth_combinations
+
 # /bin/bash execute_experiment.sh mnist.kernel_combinations
