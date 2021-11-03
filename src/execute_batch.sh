8595f5b59c09c9a246779c72459173897fb1ec4d
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-11-03 12:39:03 +0000

    prepare batch run for broken experiments

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-for i in {1..2}
-do
-    /bin/bash execute_experiment.sh mnist.dim_prog_combinations
-    /bin/bash execute_experiment.sh mnist.filter_prog_combinations
-    /bin/bash execute_experiment.sh mnist.depth_combinations
-done
+# for i in {1..2}
+# do
+#     /bin/bash execute_experiment.sh mnist.dim_prog_combinations
+#     /bin/bash execute_experiment.sh mnist.filter_prog_combinations
+#     /bin/bash execute_experiment.sh mnist.depth_combinations
+# done
 # /bin/bash execute_experiment.sh mnist.depth_scaling
 
 # /bin/bash execute_experiment.sh mnist.kernel_combinations
+
+/bin/bash execute_experiment.sh cifar.filter_prog_combinations
+/bin/bash execute_experiment.sh mnist.depth_combinations
