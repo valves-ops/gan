111fd81411f1d9b6fd87badd2f5e04cfc582957a
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-11-05 23:48:13 +0000

    rearranged experiments

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -6,9 +6,11 @@
 #     /bin/bash execute_experiment.sh mnist.filter_prog_combinations
 #     /bin/bash execute_experiment.sh mnist.depth_combinations
 # done
-# /bin/bash execute_experiment.sh mnist.depth_scaling
 
-# /bin/bash execute_experiment.sh mnist.kernel_combinations
+# /bin/bash execute_experiment.sh mnist.capacity_scaling
+# /bin/bash execute_experiment.sh mnist.dim_prog_combinations
+# /bin/bash execute_experiment.sh mnist.filter_prog_combinations
+# /bin/bash execute_experiment.sh mnist.depth_combinations
 
 /bin/bash execute_experiment.sh cifar.dim_prog_combinations
 /bin/bash execute_experiment.sh cifar.filter_prog_combinations
