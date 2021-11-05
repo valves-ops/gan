1ac10df619772defac60081840915344125211f4
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-11-05 23:57:13 +0000

    update mnist generator depth

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -7,11 +7,11 @@
 #     /bin/bash execute_experiment.sh mnist.depth_combinations
 # done
 
-# /bin/bash execute_experiment.sh mnist.capacity_scaling
-# /bin/bash execute_experiment.sh mnist.dim_prog_combinations
+/bin/bash execute_experiment.sh mnist.capacity_scaling
+/bin/bash execute_experiment.sh mnist.dim_prog_combinations
 # /bin/bash execute_experiment.sh mnist.filter_prog_combinations
 # /bin/bash execute_experiment.sh mnist.depth_combinations
 
-/bin/bash execute_experiment.sh cifar.dim_prog_combinations
-/bin/bash execute_experiment.sh cifar.filter_prog_combinations
-/bin/bash execute_experiment.sh cifar.depth_combinations
+# /bin/bash execute_experiment.sh cifar.dim_prog_combinations
+# /bin/bash execute_experiment.sh cifar.filter_prog_combinations
+# /bin/bash execute_experiment.sh cifar.depth_combinations
