cbf23c30f975a09b844caac99463180a5f57f872
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-11-06 23:28:57 +0000

    update mnist generator tpd

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -2,15 +2,13 @@
 
 # for i in {1..2}
 # do
-#     /bin/bash execute_experiment.sh mnist.dim_prog_combinations
-#     /bin/bash execute_experiment.sh mnist.filter_prog_combinations
 #     /bin/bash execute_experiment.sh mnist.depth_combinations
 # done
 
-/bin/bash execute_experiment.sh mnist.capacity_scaling
-/bin/bash execute_experiment.sh mnist.dim_prog_combinations
+# /bin/bash execute_experiment.sh mnist.capacity_scaling
+# /bin/bash execute_experiment.sh mnist.dim_prog_combinations
 # /bin/bash execute_experiment.sh mnist.filter_prog_combinations
-# /bin/bash execute_experiment.sh mnist.depth_combinations
+/bin/bash execute_experiment.sh mnist.depth_combinations
 
 # /bin/bash execute_experiment.sh cifar.dim_prog_combinations
 # /bin/bash execute_experiment.sh cifar.filter_prog_combinations
