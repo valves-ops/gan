5d76f761075a34824928bae55eb264b9a094dfd8
viniciusalves <vinicius.alves.contato@gmail.com>
2021-11-07 11:34:55 -0300

    update for cifar capacity scaling run with alpha=0.0001

--- src/execute_batch.sh
+++ src/execute_batch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# for i in {1..2}
-# do
-#     /bin/bash execute_experiment.sh mnist.depth_combinations
-# done
+for i in {1..2}
+do
+    /bin/bash execute_experiment.sh cifar.capacity_scaling
+    # /bin/bash execute_experiment.sh mnist.depth_combinations
+done
 
-/bin/bash execute_experiment.sh mnist.capacity_scaling
+# /bin/bash execute_experiment.sh mnist.capacity_scaling
 # /bin/bash execute_experiment.sh mnist.dim_prog_combinations
 # /bin/bash execute_experiment.sh mnist.filter_prog_combinations
 # /bin/bash execute_experiment.sh mnist.depth_combinations
